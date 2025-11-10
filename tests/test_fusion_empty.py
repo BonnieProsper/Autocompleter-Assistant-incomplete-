4892f9cdb46ec459c81ff203bd0a1f3e68f2a20f
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-11-10 14:36:22 +1300

    Update test_fusion_empty.py

--- tests/test_fusion_empty.py
+++ tests/test_fusion_empty.py
@@ -1,1 +1,8 @@
+# tests/test_fusion_empty.py
+from intelligent_autocompleter.core.fusion_ranker import FusionRanker
+
+def test_fusion_empty():
+    r = FusionRanker()
+    out = r.rank({}, {}, {}, {}, topn=5)
+    assert out == []
 
