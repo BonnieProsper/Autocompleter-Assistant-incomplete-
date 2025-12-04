03277cbae21f468e49e6310edcb95f259628bef8
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-12-04 16:49:05 +1300

    Update __init__.py

--- intelligent_autocompleter/cli/__init__.py
+++ intelligent_autocompleter/cli/__init__.py
@@ -1,6 +1,5 @@
-from intelligent_autocompleter.plugins import PluginLoader, PluginRegistry
-plugins_dir = os.path.join(os.path.dirname(__file__), "plugins", "examples")
-self.plugin_registry = PluginRegistry()
-self.plugin_loader = PluginLoader(plugins_dir, registry=self.plugin_registry)
-self.plugin_loader.load_all()
+# cli/__init__.py
 
+from .cli import main
+
+__all__ = ["main"]
